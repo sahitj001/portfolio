7a236f6b0d981d6cc2835754a36a7268c2defe14
sahitj001 <jordy.sahit@live.nl>
2021-02-25 13:05:43 +0100

    added more content on page, responsiveness was bit iffy so i fixed it

--- src/work-items/Tcg.jsx
+++ src/work-items/Tcg.jsx
@@ -50,6 +50,17 @@ export function Tcg() {
 
 			<div className={styles.column}>
 				<section className={styles.content}>
+				<div>
+						<h3>My role</h3>
+						<p>UI/UX</p>
+						<p>WORKED ON: Desk research, User Research, Concepting, Testing, Prototyping</p>
+
+					</div>
+
+					<div>
+					<h3>Type of product</h3>
+						<p>Physical product</p>
+					</div>
 
 					<div>
 						<h3>The problem</h3>
@@ -62,7 +73,8 @@ export function Tcg() {
 					<div>
 						<h3>Design Challenge</h3>
 						<p>Design a product that positively stimulates the gaming behaviour of children between
-						six and twelve years old. It was quite a challenge to design the UX of this project.
+						six and twelve years old. It was quite a challenge to design the UX of this project. This was
+						also my first time designing a physical product so there was a lot to learn.
 								</p>
 					</div>
 
@@ -81,47 +93,42 @@ export function Tcg() {
 					<div>
 						<h3>Setting up the chicken</h3>
 						<p>
-						When the chicken is powered on, you will start with setting up the security settings.
-						This will be done by registering your fingerprint. After having set up the system you are
-						able to choose between the gametimer or setting up a bonusday.
+							When the chicken is powered on, you will start with setting up the security settings.
+							This will be done by registering your fingerprint. After having set up the system you are
+							able to choose between the gametimer or setting up a bonusday.
 						</p>
 					</div>
-					<img className={styles.imageSize} src={chickenSetup} alt=""/>
+					<img className={styles.imageSize} src={chickenSetup} alt="" />
 
 					<div>
 						<h3>The chicken has feelings!</h3>
 						<p>
-						You stop the gametimer by pressing its mohawk. The Game Changer will become happy if you stop 1 hour before the timer ends.
-						You will get that one hour plus half an hour extra that you can use for a bonusdays. When you stop half an hour between an
-						hour the chicken will look concerned. When the chicken is concerned you will get 15 minutes bonus. If you stop half an hour or later the chicken will be sad and you won’t get any bonushours. The numbers at the right side are the hours that have been saved up.
+							You stop the gametimer by pressing its mohawk. The Game Changer will become happy if you stop 1 hour before the timer ends.
+							You will get that one hour plus half an hour extra that you can use for a bonusdays. When you stop half an hour between an
+							hour the chicken will look concerned. When the chicken is concerned you will get 15 minutes bonus. If you stop half an hour or later the chicken will be sad and you won’t get any bonushours. The numbers at the right side are the hours that have been saved up.
 						</p>
 					</div>
-					<img className={styles.imageSize} src={chickenEmotions} alt=""/>
+					<img className={styles.imageSize} src={chickenEmotions} alt="" />
 
-					<img className={styles.imageSize} src={beakRotation} alt=""/>
+					<img className={styles.imageSize} src={beakRotation} alt="" />
 					<div>
 						<h3>Adjust the timer</h3>
 						<p>
-						You can rotate the beak to adjust the gametimer. When you want to change the selection of hours
-						and minutes, you can click once on the beak. When you are done setting up the timer, double tap
-						the beak to start.
+							You can rotate the beak to adjust the gametimer. When you want to change the selection of hours
+							and minutes, you can click once on the beak. When you are done setting up the timer, double tap
+							the beak to start.
 						</p>
 					</div>
 
-					<img className={styles.imageSize} src={chickenBonus} alt=""/>
+					<img className={styles.imageSize} src={chickenBonus} alt="" />
 					<div>
 						<h3>Bonusday</h3>
 						<p>
-						When your child has been very nice or if the holidays have started, you might consider starting a bonusday. By initiating a bonusday The Game Changer will use the spare hours that have been stored. You can start a bonusday via the main menu. The timer starts immediately.
-						 When the child is done, he can press on the mohawk to end the bonusday.
+							When your child has been very nice or if the holidays have started, you might consider starting a bonusday. By initiating a bonusday The Game Changer will use the spare hours that have been stored. You can start a bonusday via the main menu. The timer starts immediately.
+							When the child is done, he can press on the mohawk to end the bonusday.
 						</p>
 					</div>
-
 				</section>
-
-
-
-
 			</div>
 
 		</div>
